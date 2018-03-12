ebb0431bb41ae272b4fee2d61e2a181b6b6f3ada
unknown <diegoelias1@gmail.com>
2018-03-12 20:02:09 +0100

    Finishing the preprocess step of the framework

---
+++ tasks/preprocess-code.py
@@ -0,0 +1,95 @@
+import luigi
+import os
+import sys
+
+# Import parent module
+# FIXME: There must be a better way of handling this
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+from jupyter_notebook import JupyterNotebookTask
+
+notebooks_path = os.path.join(module_path, 'code', 'data-preprocess')
+data_path = os.path.join(module_path, 'data', 'stack-overflow')
+
+
+class FilterPosts(JupyterNotebookTask):
+    notebook_path = os.path.join(notebooks_path, 'posts-filter.ipynb')
+    kernel_name = 'python3'
+    timeout = 60
+
+    questions_score = luigi.Parameter(default=0)
+    answers_score = luigi.Parameter(default=0)
+    input_file = luigi.Parameter(default=os.path.join(data_path, 'pandas-posts-dataset.csv'))
+
+    def output(self):
+        return luigi.LocalTarget(os.path.join(
+            data_path, 'pandas-filteredposts-dataset.csv')
+        )
+
+		
+class ExtractCode(JupyterNotebookTask):
+    notebook_path = os.path.join(notebooks_path, 'codeblock-extractor.ipynb')
+    kernel_name = 'python3'
+    timeout = 60
+
+    def requires(self):
+        return FilterPosts()
+
+    def output(self):
+        return luigi.LocalTarget(os.path.join(
+            data_path, 'pandas-code-dataset.csv')
+        )
+
+
+class PreProcessSpecialCharsCode(JupyterNotebookTask):
+    notebook_path = os.path.join(notebooks_path, 'code-preprocess-specialchars.ipynb')
+    kernel_name = 'python3'
+    timeout = 120
+    
+    input_col = luigi.Parameter(default='Code')
+    output_col = luigi.Parameter(default='PreprocessedCode')
+
+    def requires(self):
+        return ExtractCode()
+        
+    def output(self):
+        return luigi.LocalTarget(os.path.join(
+            data_path, 'pandas-preprocessedcode-dataset-part1.csv')
+        )
+		
+
+class PreProcessTerminalLikeCode(JupyterNotebookTask):
+    notebook_path = os.path.join(notebooks_path, 'code-preprocess-terminalcode.ipynb')
+    kernel_name = 'python3'
+    timeout = 120
+    
+    input_col = luigi.Parameter(default='PreprocessedCode')
+    output_col = luigi.Parameter(default='PreprocessedCode')
+
+    def requires(self):
+        return PreProcessSpecialCharsCode()
+        
+    def output(self):
+        return luigi.LocalTarget(os.path.join(
+            data_path, 'pandas-preprocessedcode-dataset-part2.csv')
+        )
+		
+		
+class PreProcessParseableCode(JupyterNotebookTask):
+    notebook_path = os.path.join(notebooks_path, 'code-preprocess-parseablecode.ipynb')
+    kernel_name = 'python3'
+    timeout = 120
+    
+    input_col = luigi.Parameter(default='PreprocessedCode')
+    output_col = luigi.Parameter(default='PreprocessedCode')
+
+    def requires(self):
+        return PreProcessTerminalLikeCode()
+        
+    def output(self):
+        return luigi.LocalTarget(os.path.join(
+            data_path, 'pandas-preprocessedcode-dataset-done.csv')
+        )
+		
