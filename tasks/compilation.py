b6d34ecbf06368e043c791fa07844f7ee18da51a
unknown <diegoelias1@gmail.com>
2018-03-12 15:15:33 +0100

    Updating Task

--- tasks/compilation.py
+++ tasks/compilation.py
@@ -10,8 +10,8 @@ if module_path not in sys.path:
 
 from jupyter_notebook import JupyterNotebookTask
 
-notebooks_path = os.path.join('..', 'data-exploration')
-data_path = os.path.join('..', 'data')
+notebooks_path = os.path.join('..', 'code', 'preprocessing')
+data_path = os.path.join('..', 'stack-overflow', 'data')
 
 
 class FilterPosts(JupyterNotebookTask):
