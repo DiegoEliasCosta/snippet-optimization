1ea24d66af9ef560ad354254a387bf68a9b01398
unknown <diegoelias1@gmail.com>
2018-03-19 15:40:37 +0100

    Updating H2 and H1H2

--- tasks/evaluate.py
+++ tasks/evaluate.py
@@ -21,8 +21,12 @@ results_path = os.path.join(module_path, 'data', 'results')
 api_doc_file = os.path.join(module_path, 'data-import', 'build_api_doc_base', 'api_doc.csv') 
 
 
+# ------------------------------------------------
+#  						H1  
+# ------------------------------------------------
+
 class RunH1(JupyterNotebookTask):
-    notebook_path = os.path.join(analyze_notebook_path, 'H1-method_name_matching.ipynb')
+    notebook_path = os.path.join(method_notebook_path, 'h1', 'H1-method_name_matching.ipynb')
     kernel_name = 'python3'
     timeout = 60
 
@@ -52,3 +56,76 @@ class EvaluateH1(JupyterNotebookTask):
 
     def output(self):
         return luigi.LocalTarget(os.path.join(results_path, 'results-h1'))
+		
+		
+# ------------------------------------------------
+#  						 H2
+# ------------------------------------------------
+		
+class RunH2(JupyterNotebookTask):
+    notebook_path = os.path.join(method_notebook_path, 'h2', 'H2-Non_Contructors.ipynb')
+    kernel_name = 'python3'
+    timeout = 60
+
+    dataset = luigi.Parameter(default=os.path.join(data_path, 'Dataset - Pandas.csv'))
+
+    input_col = luigi.Parameter(default='PreprocessedCode3')    
+    api_doc_file = luigi.Parameter(default=api_doc_file)   
+    
+    def requires(self):
+        return PreProcessParseableCode()
+
+    def output(self):
+        return luigi.LocalTarget(os.path.join(data_path, 'pandas-solutioncode-h2'))
+
+		
+		
+class EvaluateH2(JupyterNotebookTask):
+    notebook_path = os.path.join(analyze_notebook_path, 'evaluate.ipynb')
+    kernel_name = 'python3'
+    timeout = 60
+
+    dataset = luigi.Parameter(default=os.path.join(data_path, 'Dataset - Pandas.csv'))
+
+
+    def requires(self):
+        return RunH2()
+
+    def output(self):
+        return luigi.LocalTarget(os.path.join(results_path, 'results-h2'))
+		
+		
+# ------------------------------------------------
+#  					H1 and H2
+# ------------------------------------------------
+class RunH1H2(JupyterNotebookTask):
+    notebook_path = os.path.join(method_notebook_path, 'h2', 'H1H2-Non_Contructors.ipynb')
+    kernel_name = 'python3'
+    timeout = 60
+
+    dataset = luigi.Parameter(default=os.path.join(data_path, 'Dataset - Pandas.csv'))
+
+    input_col = luigi.Parameter(default='PreprocessedCode3')    
+    api_doc_file = luigi.Parameter(default=api_doc_file)   
+    
+    def requires(self):
+        return PreProcessParseableCode()
+
+    def output(self):
+        return luigi.LocalTarget(os.path.join(data_path, 'pandas-solutioncode-h1h2'))
+
+		
+		
+class EvaluateH1H2(JupyterNotebookTask):
+    notebook_path = os.path.join(analyze_notebook_path, 'evaluate.ipynb')
+    kernel_name = 'python3'
+    timeout = 60
+
+    dataset = luigi.Parameter(default=os.path.join(data_path, 'Dataset - Pandas.csv'))
+
+
+    def requires(self):
+        return RunH1H2()
+
+    def output(self):
+        return luigi.LocalTarget(os.path.join(results_path, 'results-h1h2'))
