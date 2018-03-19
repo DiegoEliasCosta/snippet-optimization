f2c469fc8e27883ab4fe3780ec3440be471e9aeb
unknown <diegoelias1@gmail.com>
2018-03-19 15:00:54 +0100

    Updating the evaluation + implementation of H1 code

--- tasks/evaluate.py
+++ tasks/evaluate.py
@@ -1,7 +1,8 @@
 import luigi
 import os
 import sys
-from preprocess-code import PreProcessParseableCode
+
+from preprocess_code import PreProcessParseableCode
 
 # Import parent module
 # FIXME: There must be a better way of handling this
@@ -11,5 +12,43 @@ if module_path not in sys.path:
 
 from jupyter_notebook import JupyterNotebookTask
 
-notebooks_path = os.path.join(module_path, 'code', 'data-preprocess')
+method_notebook_path = os.path.join(module_path, 'code', 'method')
+analyze_notebook_path = os.path.join(module_path, 'code', 'analysis')
+
 data_path = os.path.join(module_path, 'data', 'stack-overflow')
+results_path = os.path.join(module_path, 'data', 'results')
+
+api_doc_file = os.path.join(module_path, 'data-import', 'build_api_doc_base', 'api_doc.csv') 
+
+
+class RunH1(JupyterNotebookTask):
+    notebook_path = os.path.join(analyze_notebook_path, 'H1-method_name_matching.ipynb')
+    kernel_name = 'python3'
+    timeout = 60
+
+    dataset = luigi.Parameter(default=os.path.join(data_path, 'Dataset - Pandas.csv'))
+
+    input_col = luigi.Parameter(default='PreprocessedCode3')    
+    api_doc_file = luigi.Parameter(default=api_doc_file)   
+    
+    def requires(self):
+        return PreProcessParseableCode()
+
+    def output(self):
+        return luigi.LocalTarget(os.path.join(data_path, 'pandas-solutioncode-h1'))
+
+		
+		
+class EvaluateH1(JupyterNotebookTask):
+    notebook_path = os.path.join(analyze_notebook_path, 'evaluate.ipynb')
+    kernel_name = 'python3'
+    timeout = 60
+
+    dataset = luigi.Parameter(default=os.path.join(data_path, 'Dataset - Pandas.csv'))
+
+
+    def requires(self):
+        return RunH1()
+
+    def output(self):
+        return luigi.LocalTarget(os.path.join(results_path, 'results-h1'))
